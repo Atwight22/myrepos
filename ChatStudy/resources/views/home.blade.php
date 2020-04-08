f9fd7367aedc2603414e1c743337d654851fa0ae
honoka hayashi <honoka.5398@gmail.com>
2020-04-08 19:57:14 +0900

    Laravel導入してチャットサービス作成。 ログイン、登録画面でエラー吐く。

---
+++ ChatStudy/resources/views/home.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="chat-container row justify-content-center">
+        <div class="chat-area">
+            <div class="card">
+                <div class="card-header">Comment</div>
+                    <div class="card-body chat-card">
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                        @include('components.comment')
+                    </div>
+                 </div>
+            </div>
+        </div>
+        <form method="POST" action="{{route('add')}}">
+                <div class="comment-container row justify-content-center">
+                    <div class="input-group comment-area">
+                        <textarea class="form-control" id="comment" name="comment" placeholder="input message" aria-lavel="With textarea"></textarea>
+                        <button type="input-group-prepend button" class="btn btn-outline-primary comment-btn">送信</button>
+                    </div>
+                </div>
+        </form>
+                <div class="card-body chat-card">
+                    @foreach ($comments as $item)
+                    @include('components.comment', ['item' => $item])
+                    @endforeach
+                </div>
+        
+    </div>
+@endsection
