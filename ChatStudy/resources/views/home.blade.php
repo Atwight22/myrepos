9230fe2a49725ca242058ab744714f5f411a8e98
honoka hayashi <honoka.5398@gmail.com>
2020-04-09 20:23:31 +0900

    アカウント登録→ログイン→チャット画面表示実装。

--- ChatStudy/resources/views/home.blade.php
+++ ChatStudy/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="chat-container row justify-content-center">
         <div class="chat-area">
             <div class="card">
-                <div class="card-header">Comment</div>
+                <div class="card-header">チャット</div>
                     <div class="card-body chat-card">
                         @include('components.comment')
                         @include('components.comment')
@@ -30,6 +30,8 @@
         <form method="POST" action="{{route('add')}}">
                 <div class="comment-container row justify-content-center">
                     <div class="input-group comment-area">
+                        <input type="file" name="file" style="display:none"></input>
+                        <button id="upload_chat_button" class="btn btn-small btn-inverse"><i class="icon-upload icon-white"></i></button>
                         <textarea class="form-control" id="comment" name="comment" placeholder="input message" aria-lavel="With textarea"></textarea>
                         <button type="input-group-prepend button" class="btn btn-outline-primary comment-btn">送信</button>
                     </div>
---
+++ ChatStudy/resources/views/layouts/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="chat-container row justify-content-center">
+    <div class="chat-area">
+        <div class="card">
+            <div class="card-header">Comment</div>
+            <div class="card-body chat-card">
+            @foreach ($comments as $item)
+            @include('components.comment', ['item' => $item])
+            @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="comment-container row justify-content-center">
+    <div class="input-group comment-area">
+        <textarea class="form-control" placeholder="input massage" aria-label="With textarea"></textarea>
+        <button type="input-group-prepend button" class="btn btn-outline-primary comment-btn">Submit</button>
+    </div>
+</div>
+
+@endsection
