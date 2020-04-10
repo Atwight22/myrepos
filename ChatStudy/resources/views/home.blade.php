c0555db76821ae8f63175b406523fb3e3a86a932
honoka hayashi <honoka.5398@gmail.com>
2020-04-10 18:42:46 +0900

    チャット画面でメッセージ送信機能実施。

--- ChatStudy/resources/views/home.blade.php
+++ ChatStudy/resources/views/home.blade.php
@@ -6,42 +6,26 @@
         <div class="chat-area">
             <div class="card">
                 <div class="card-header">チャット</div>
-                    <div class="card-body chat-card">
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                        @include('components.comment')
-                    </div>
-                 </div>
-            </div>
-        </div>
-        <form method="POST" action="{{route('add')}}">
-                <div class="comment-container row justify-content-center">
-                    <div class="input-group comment-area">
-                        <input type="file" name="file" style="display:none"></input>
-                        <button id="upload_chat_button" class="btn btn-small btn-inverse"><i class="icon-upload icon-white"></i></button>
-                        <textarea class="form-control" id="comment" name="comment" placeholder="input message" aria-lavel="With textarea"></textarea>
-                        <button type="input-group-prepend button" class="btn btn-outline-primary comment-btn">送信</button>
-                    </div>
-                </div>
-        </form>
                 <div class="card-body chat-card">
                     @foreach ($comments as $item)
                     @include('components.comment', ['item' => $item])
                     @endforeach
                 </div>
+                </div>
+            </div>
+        </div>
+        <form method="POST" action="{{route('add')}}">
+    @csrf
+    <div class="comment-container row justify-content-center">
+        <div class="input-group comment-area">
+        {{--SHIFT + ENTERでも送信可能--}} 
+            <textarea class="form-control" id="comment" name="comment" placeholder="push massage (shift + Enter)"
+                aria-label="With textarea"
+                onkeydown="if(event.shiftKey&&event.keyCode==13){document.getElementById('submit').click();return false};"></textarea>
+            <button type="submit" id="submit" class="btn btn-outline-primary comment-btn">Submit</button>
+        </div>
+    </div>
+</form>
         
     </div>
 @endsection
