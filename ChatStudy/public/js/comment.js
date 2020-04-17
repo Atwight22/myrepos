0edae6a9d1cdd577248471e315626a21889e9e2d
honoka hayashi <honoka.5398@gmail.com>
2020-04-17 19:11:23 +0900

    新着コメントの並び替えを降順にした。

--- ChatStudy/public/js/comment.js
+++ ChatStudy/public/js/comment.js
@@ -6,12 +6,13 @@ function get_data() {
     $.ajax({
         url: "result/ajax/",
         dataType: "json",
+        // 成功した場合
         success: data => {
             $("#comment-data")
                 .find(".comment-visible")
                 .remove();
-        
-            for (var i = 0; i < data.comments.length; i++) {
+            // 新着コメントを最下位表示にする
+            for (var i = data.comments.length-1; i >= 0;  i--) {
                 var html = `
                             <div class="media comment-visible">
                                 <div class="media-body comment-body">
